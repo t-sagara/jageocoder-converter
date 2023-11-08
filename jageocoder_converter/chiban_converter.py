b0e28256be194de233bab5ca49e24ede9b415e49
Takeshi Sagara <sagara@info-proto.com>
2023-11-08 21:11:40 +0900

    Changed input chiban-file format.
    - Changed the format of chiban-files from gzip to zip.

--- jageocoder_converter/chiban_converter.py
+++ jageocoder_converter/chiban_converter.py
@@ -1,12 +1,11 @@
 import csv
-import gzip
 import io
 from logging import getLogger
 import os
 from typing import Union, Optional, List
+import zipfile
 
 from jageocoder.address import AddressLevel
-from jageocoder.node import AddressNode
 
 from jageocoder_converter import BaseConverter
 from jageocoder_converter.data_manager import DataManager
@@ -21,7 +20,7 @@ class ChibanConverter(BaseConverter):
 
     Output 'output/xx_chiban.txt' for each prefecture.
     """
-    dataset_name = "法務省登記所備付地図データ"
+    dataset_name = "法務省登記所備付地図代表点データ"
     dataset_url = "https://front.geospatial.jp/moj-chizu-xml-readme/"
 
     def __init__(self,
@@ -42,7 +41,7 @@ class ChibanConverter(BaseConverter):
         Show the terms of the license agreement and confirm acceptance.
         """
         terms = (
-            "「法務省登記所備付地図データ」をダウンロードします。\n"
+            "「法務省登記所備付地図代表点データ」をダウンロードします。\n"
             "利用条件等は {url} を確認してください。\n"
         ).format(url='https://www.geospatial.jp/ckan/dataset/houmusyouchizu-2022-1-1/resource/737ca982-2217-4721-8be4-eb5bf9cb1899')  # noqa: E501
         return super().confirm(terms)
@@ -55,7 +54,7 @@ class ChibanConverter(BaseConverter):
         version = '2022'  # PY2022, 令和4年度
         urls = []
         for pref_code in self.targets:
-            url = "{0}/{1}/{2}_chiban.csv.gz".format(
+            url = "{0}/{1}/{2}_chiban.zip".format(
                 urlbase, version, pref_code)
             urls.append(url)
 
@@ -64,46 +63,54 @@ class ChibanConverter(BaseConverter):
             dirname=self.input_dir
         )
 
-    def add_from_gzfile(self, gzfilepath):
-        with gzip.open(gzfilepath, 'rb') as fb:
-            ft = io.TextIOWrapper(
-                fb, encoding='UTF-8', newline='',
-                errors='backslashreplace')
-            reader = csv.reader(ft)
-            logger.debug('Processing {}...'.format(gzfilepath))
-            for row in reader:
-                if row[0][0] in ('市', '(',):
+    def add_from_zipfile(self, zipfilepath):
+        """
+        Register address notations from 登記所備付地図代表点データ
+        """
+        with zipfile.ZipFile(zipfilepath) as z:
+            for filename in z.namelist():
+                if not filename.lower().endswith('.csv'):
                     continue
 
-                names = self.jiscodes[row[0]][:]  # 市町村c
-                if row[1]:  # 大字名
-                    names.append([AddressLevel.OAZA, row[1]])
-                else:  # 大字なし
-                    names.append([AddressLevel.OAZA, AddressNode.NONAME])
+                with z.open(filename, mode='r') as f:
+                    ft = io.TextIOWrapper(
+                        f, encoding='UTF-8', newline='',
+                        errors='backslashreplace')
+                    reader = csv.reader(ft)
+                    pre_args = None
+                    logger.debug('Processing {} in {}...'.format(
+                        filename, zipfilepath))
+                    for row in reader:
+                        if row[0][0] in ('市', '(',):  # Skip header line
+                            continue
 
-                if row[2]:  # 丁目名
-                    names.append([AddressLevel.AZA, row[2]])
+                        names = self.jiscodes[row[0]][:]  # 市町村c
+                        if row[1]:  # 大字名
+                            names.append([AddressLevel.OAZA, row[1]])
 
-                if row[3]:  # 小字名
-                    names.append([AddressLevel.AZA, row[3]])
+                        if row[2]:  # 丁目名
+                            names.append([AddressLevel.AZA, row[2]])
 
-                if row[4]:  # 予備名
-                    names.append([AddressLevel.AZA, row[4]])
+                        if row[3]:  # 小字名
+                            names.append([AddressLevel.AZA, row[3]])
 
-                if row[5]:  # 地番
-                    ban_list = row[5].split('-')
-                    if len(ban_list) > 0:
-                        names.append(
-                            [AddressLevel.BLOCK, ban_list[0] + '番地'])
+                        if row[4]:  # 予備名
+                            names.append([AddressLevel.AZA, row[4]])
 
-                    for ban in ban_list[1:]:
-                        names.append([AddressLevel.BLD, ban])
+                        if row[5]:  # 地番
+                            ban_list = row[5].split('-')
+                            if len(ban_list) > 0:
+                                names.append(
+                                    [AddressLevel.BLOCK, ban_list[0] + '番地'])
 
-                self.print_line(names, row[6], row[7])
+                            for ban in ban_list[1:]:
+                                names.append([AddressLevel.BLD, ban])
+
+                        self.print_line(names, row[6], row[7])
 
     def convert(self):
         """
-        Read records from 'chiban/xx_chiban.csv.gz' files, format them,
+        Read records from 'chiban/xx_chiban.zip' files, format them,
         then output to 'output/xx_chiban.txt'.
         """
         self.prepare_jiscode_table()
@@ -116,9 +123,9 @@ class ChibanConverter(BaseConverter):
                 continue
 
             input_filepath = os.path.join(
-                self.input_dir, '{}_chiban.csv.gz'.format(pref_code))
+                self.input_dir, '{}_chiban.zip'.format(pref_code))
 
             with open(output_filepath, 'w', encoding='utf-8') as fout:
                 self.set_fp(fout)
                 logger.debug("Reading from {}".format(input_filepath))
-                self.add_from_gzfile(input_filepath)
+                self.add_from_zipfile(input_filepath)
