49f35e1761ff779682e513ca22160f68d75962b3
Takeshi Sagara <sagara@info-proto.com>
2024-01-07 03:44:00 +0000

    Added index.html of the gh-pages.

--- build_datafiles.py
+++ build_datafiles.py
@@ -167,9 +167,9 @@ def filelist_html(base_db_dir: Path) -> str:
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>Jageocoder Datafile list</title>
     <style type="text/css">
-    .mono {{
+    .mono {
         font-family: monospace, serif;
-    }}
+    }
     </style>
   </head>
   <body>
